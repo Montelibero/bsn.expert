e7698e9e51d3043c3d28dcb440172b18958db4f8
Soz Nov <sozidatel@gmail.com>
2026-05-05 11:37:52 +0200

    Add legacy `/editor/{legacy_source_id}` route to `Editor2Router` for backward compatibility

--- app/classes/Montelibero/BSN/Routes/Editor2Router.php
+++ app/classes/Montelibero/BSN/Routes/Editor2Router.php
@@ -13,5 +13,10 @@ class Editor2Router
         SimpleRouter::match(['get', 'post'], '/', function () use ($Container) {
             return $Container->get(Editor2Controller::class)->Editor();
         });
+
+        // Legacy /editor compatibility: old direct edit links used /editor/{source_account_id}/.
+        SimpleRouter::match(['get', 'post'], '/{legacy_source_id}', function ($legacy_source_id) use ($Container) {
+            return $Container->get(Editor2Controller::class)->LegacyPathRedirect($legacy_source_id);
+        });
     }
 }
